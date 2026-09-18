48a1a892588ad3a09b0bdef3c930c56a7907d45a
aajkrvv <aajkrvv@gmail.com>
2026-09-18 17:00:53 +0900

    add script file

---
+++ grass block.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          grass block
+// @namespace     https://github.com/aajkrvv/grass-block
+// @version       0.1.0
+// @description   site blocking script
+// @author        aajkrvv
+// @license       MIT
+// @homepageURL   https://aajkrvv.github.io/grass-block
+// @source        https://github.com/aajkrvv/grass-block
+// @supportURL    https://github.com/aajkrvv/grass-block/issues
+// @match         *://*/*
+// @run-at        document-start
+// ==/UserScript==
